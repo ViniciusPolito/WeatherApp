ceaec4058bf0211c996db06459ecfccbc510332a
ViniciusPolito <vini.barpo@gmail.com>
2022-09-04 21:52:01 -0300

    Added Simple Weather Consumer Api

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useMemo, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [cidade, setCidade] = useState("");
+  const [temperatura, setTemperatura] = useState("0");
+  const [clima, setClima] = useState("none");
+
+  const requestDataWeather = async () => {
+    try {
+
+      const response = await fetch(`https://weather.contrateumdev.com.br/api/weather/city/?city=${cidade}`);
+      const json = await response.json();
+      setTemperatura(json.main.temp)
+      setClima(json.weather[0].description)
+
+      if (response.ok) {
+        setTemperatura(json.main.temp)
+        setClima(json.weather[0].description)
+      } else {
+        Alert.alert("Falha na requisição", "Error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Cidade: {cidade}</Text>
+      <Text>Temperatura: {temperatura}ºC</Text>
+      <Text>Clima: {clima}</Text>
       <StatusBar style="auto" />
+
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite a cidade..."
+        style={styles.input}
+        onChangeText={cidade => setCidade(cidade)}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={requestDataWeather}
+      >
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -16,5 +60,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#38A69D',
+    borderRadius: 4,
+    paddingVertical: 8,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 14
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#FFF',
+    fontWeight: 'bold'
   },
 });
